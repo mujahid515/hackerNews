63e55d278810d0720671fde1f5ae9c3d3da4004b
Mujahid <mujahid.chowdhury515@gmail.com>
2018-10-02 22:43:04 +0100

    karma tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule }  from '@angular/http';
 import { AppComponent } from './app.component';
+import { DataService } from './services/data.service';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        HttpModule
+      ],
+      providers: [DataService],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +23,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Hacker News App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Hacker News App');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the');
   }));
 });
